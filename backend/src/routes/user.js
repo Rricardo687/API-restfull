c5963451d45fe81796ef9895e5f19bd16507fe7e
Your Name <you@example.com>
2022-05-10 11:44:40 -0500

    crud terminado

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -5,6 +5,7 @@ const userSchema = require('../models/user');
    // create user // routes 
          // C R U D 
         // create user / crear  usuario
+        
   router.post('/users', (request, response) => {
    const user = userSchema(request.body);
    user
@@ -12,7 +13,9 @@ const userSchema = require('../models/user');
    .then((data) => response.json({ success: data }))
    .catch((err) => response.json({ failured: err }));
   });
+
         // get all users / mostras todos los usuarios
+
  router.get('/users', (request, response) => {
   const user = userSchema(request.body);
   userSchema
@@ -30,13 +33,25 @@ const userSchema = require('../models/user');
   .then((data) => response.json({ success: data }))
   .catch((err) => response.json({ failured: err }));
  });
+            // update a user // actualizar un usuario
 
  router.put('/users/:id', (request, response) => {
-   response.send('modificar un uruario');
+  const {id} = request.params;
+  const{name,age,email} = request.body;
+  userSchema
+  .updateOne({_id: id },{$set: {name,age,email}})
+  .then((data) => response.json({ success: data }))
+  .catch((err) => response.json({ failured: err }));
  });
+ 
+            // delete user // eliminar usuario
 
  router.delete('/users/:id', (request, response) => {
-   response.send('eliminar usuario');
+  const {id} = request.params;
+  userSchema
+  .deleteOne({_id: id })
+  .then((data) => response.json({ success: data }))
+  .catch((err) => response.json({ failured: err }));
  });
 
 
